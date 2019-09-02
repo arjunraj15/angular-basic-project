667ef790cc2531f624d3209adfcaca2a2fd4f91a
arjunraj15 <arjunrajvcet@gmail.com>
2019-09-02 08:21:58 +0530

    1 file modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
 
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeslistComponent } from './recipes/recipeslist/recipeslist.component';
+import { RecipesdetailsComponent } from './recipes/recipesdetails/recipesdetails.component';
+import { RecipeitemComponent } from './recipes/recipeitem/recipeitem.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent,  RecipesComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent,  RecipesComponent, RecipeslistComponent, RecipesdetailsComponent, RecipeitemComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/recipes/recipesdetails/recipesdetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipesdetails',
+  templateUrl: './recipesdetails.component.html',
+  styleUrls: ['./recipesdetails.component.css']
+})
+export class RecipesdetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
