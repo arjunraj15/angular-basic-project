9b493da31824595687d36fc3569826af6e037973
arjunraj15 <arjunrajvcet@gmail.com>
2019-09-05 19:01:16 +0530

    1 file added

---
+++ src/app/recipes/recipes.model.ts
@@ -0,0 +1,11 @@
+export class recipe {
+  public name: string;
+  public discription: string;
+  public imagepath: string;
+  constructor( name: string,discription: string,imagepath: string)
+  {
+    this.name = name;
+    this.discription = discription;
+    this.imagepath = imagepath;
+  }
+}
