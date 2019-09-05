15731b9524a4d65de4e9d5245250004f671fcaa8
arjunraj15 <arjunrajvcet@gmail.com>
2019-09-05 19:11:44 +0530

    1 file modified

--- src/app/recipes/recipeslist/recipeslist.component.ts
+++ src/app/recipes/recipeslist/recipeslist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { recipie } from '../recipes.model';
 
 @Component({
   selector: 'app-recipeslist',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeslistComponent implements 
 OnInit {
-  recipes = [];
+  recipes: recipie[] = [];
 
   constructor() { }
 
