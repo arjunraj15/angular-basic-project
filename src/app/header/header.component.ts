1cd825334548237c426cff228c31284c2439dce7
arjunraj15 <arjunrajvcet@gmail.com>
2019-09-21 11:53:31 +0530

    4 files modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-head',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+@Output() display1 =new EventEmitter <string>();
   constructor() { }
 
   ngOnInit() {
+  }
+  onclick(show:string)
+  {
+    this.display1.emit(show);
+
   }
 
 }
